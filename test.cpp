a18bd17f45a9f9b62084abbd023458a8feba8f42
lovechen <1156841766@qq.com>
2022-08-20 23:43:09 +0800

    0.2

--- test.cpp
+++ test.cpp
@@ -2,13 +2,13 @@
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-08-20 16:45:13
  * @LastEditors: lovechen 1156841766@qq.com
- * @LastEditTime: 2022-08-20 23:33:08
+ * @LastEditTime: 2022-08-20 23:43:03
  * @FilePath: /undefined/Users/chiyi/c++练习/未命名文件夹/test.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include <iostream>
 #include <string>
-#include <fstream>
+
 
 using namespace std;
 void swap(int *x,int *y);
