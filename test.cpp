fd75ede9430024a8c1ee9fd5c54f3fb940080bd9
lovechen <1156841766@qq.com>
2022-08-22 22:38:20 +0800

    gai

--- test.cpp
+++ test.cpp
@@ -1,30 +1,29 @@
-/*
- * @Author: error: git config user.name && git config user.email & please set dead value or install git
- * @Date: 2022-08-20 16:45:13
- * @LastEditors: lovechen 1156841766@qq.com
- * @LastEditTime: 2022-08-21 23:54:00
- * @FilePath: /undefined/Users/chiyi/c++练习/未命名文件夹/test.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-#include <iostream>
-#include <string>
-#include <fstream>
+#include<iostream>
 
-using namespace std;
-void swap(int *x,int *y);
-
-class Car
+class Point
 {
+    private:
+        int x,y;
     public:
-        string color;
-        string engine;
-        float gas_tank;
-        unsigned int wheel;
-        
+        Point(int a,int b)
+        {
+            x=a;
+            y=b;
+        }
+        void MovePoint(int a,int b)
+        {
+            x=a;
+            y=b;
+        }
+        void print()
+        {
+            std::cout << "x = " << x << "y = "<<y<<std::endl;
+        }
 };
 
 int main()
 {
-
-    return 0;
+    Point point1(10,10);
+    point1.MovePoint(2,2);
+    point1.print();
 }
